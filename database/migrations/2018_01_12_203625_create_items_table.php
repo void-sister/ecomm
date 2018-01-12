67b7fe688464a0f7125a1406edae44b7a4a3baad
Varya <el.varvara.el@gmail.com>
2018-01-12 23:55:34 +0200

    Options and Items Migrations

--- database/migrations/2018_01_12_203625_create_items_table.php
+++ database/migrations/2018_01_12_203625_create_items_table.php
@@ -13,27 +13,25 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('item_types', function (Blueprint $table) {
-            $table->increments('id');
-            $table->char('item_type', 25);
-        });
-        
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('item_type_id')->unsigned();
             $table->char('item_name', 25);
             $table->unsignedInteger('item_price')->default(0);
-            $table->unsignedInteger('item_qty')->default(0);
+            $table->integer('category_id')->unsigned();
             $table->text('item_description')->nullable();
             $table->text('other_item_details')->nullable();
 
-            $table->foreign('item_type_id')
-                ->references('id')->on('item_types')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories');
+            
         });
-    }
 
+        
+        if (Schema::hasTable('models')) {
+            Schema::table('models', function (Blueprint $table) {
+                $table->foreign('item_id')->references('id')->on('items');
+            });
+        }       
+    } 
 
     /**
      * Reverse the migrations.
@@ -42,7 +40,8 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('item_types');        
         Schema::dropIfExists('items');
     }
 }
+
+
