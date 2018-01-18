d9448a1ecfcf8f64986705955ee63bc352d53195
Varya <el.varvara.el@gmail.com>
2018-01-18 10:49:36 +0200

    Create create view for category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -26,7 +26,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        
+        return view('categories.create');
     }
 
     /**
---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('header')
+<h1> Category Create Page</h1>
+@endsection
+
+@section('content')
+
+
+<br><a href="{{ URL::to('categories/') }}">Back</a>
+@endsection 
